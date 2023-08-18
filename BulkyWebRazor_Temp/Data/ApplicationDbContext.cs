12c586b0e10e98de73375c99e6026cdc411be437
Mustafa Ghnaim <44782684+mut1234@users.noreply.github.com>
2023-08-18 07:39:18 +0300

    Razer Page Project

---
+++ BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Category> Categories { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+
+			modelBuilder.Entity<Category>().HasData(
+				new Category { CategoryId = 1, Name = "Action", DisplayOrder = 1 },
+				new Category { CategoryId = 2, Name = "Scifi", DisplayOrder = 2 },
+				new Category { CategoryId = 3, Name = "History", DisplayOrder = 3 }
+				);
+		}
+	}
+}
